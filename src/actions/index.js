235da9c099d1c964f58b217eb2ea67d941492310
ycbencheng <ycbencheng@gmail.com>
2020-04-23 10:05:10 -0600

    Setup json placeholder api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPost = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPost = async () => {
+  const response = await jsonPlaceholder.get("/posts");
+
   return {
     type: "FETCH_POSTS",
+    payload: response,
   };
 };
